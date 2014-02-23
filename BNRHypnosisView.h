45c9533b5e70d4e23ba379294c5aaa3c96194ed5
Justin Steffen <JustinDSN@gmail.com>
2014-02-23 14:21:28 -0800

    Finishing chapter 4.

---
+++ BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Justin Steffen on 2/23/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
